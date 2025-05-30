76040b0052697a849e83a7431e3d43311c3914e1
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-05-30 04:23:05 +0200

    expression inclusing ternary operator

--- Expressions.php
+++ Expressions.php
@@ -1,3 +1,20 @@
 <?php
 
 echo "Welcome to PHP\n";
+
+$name = "Alice";
+
+echo "Hello, ". $name . "!\n";
+
+$pizzas = 3;
+$slicesPerPizzas = 8;
+$totalSlices = $pizzas * $slicesPerPizzas;
+
+echo "Total pizza slicers: " .$totalSlices. "!\n";
+
+$isHungry = true;
+echo "Hungry?";
+
+echo $isHungry ? "Yes" : "No" ;
+
+echo "\n";
