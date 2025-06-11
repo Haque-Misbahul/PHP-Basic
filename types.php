b80d69493b0fffd1fc7c7e678bcb9dd983295c6d
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-06-11 22:15:35 +0200

    single/double quote

--- types.php
+++ types.php
@@ -8,4 +8,9 @@ var_dump($isStudent, true, $isStudent == true);
 
 $scores = [85, (int)"95.5"];
 
-var_dump($scores, $scores[0] +$scores[1]);
+$total = $scores[0] +$scores[1];
+
+var_dump($scores, $total);
+
+echo "Total score is : ". $total . "\n";
+// within single quote varialbe and special does not work
