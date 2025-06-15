adbcf1a13688460bc1435df5a698a79d03ea6de2
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-06-15 21:05:23 +0200

    else if statement

--- if.php
+++ if.php
@@ -7,3 +7,16 @@ if ($x > 5) {
    echo "x is greater that 5 \n";
 
 }
+$score = 85;
+
+if ($score >= 90) {
+   echo "A";  
+} elseif ($score >= 80) {
+   echo "B";
+}elseif ($score >= 70) {
+   echo "C";
+}elseif ($score >= 60) {
+   echo "D";
+}else {
+   echo "F";
+}
