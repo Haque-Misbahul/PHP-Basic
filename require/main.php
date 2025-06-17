518d7cbb8f44e84d16debc90be8d073180fbe11c
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-06-17 23:16:20 +0200

    include/require

---
+++ require/main.php
@@ -0,0 +1,8 @@
+<?php
+
+// include "config.php";
+require "config.php";
+// include: warning if missing
+// require: fatal error if missing
+// include_once: include only once
+echo "Database $dbHost:$dbUser";
