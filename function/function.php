f01ad6704eae10279f648da571d6ac39fa23bbb7
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-06-18 23:31:15 +0200

    function with optional argument

--- function/function.php
+++ function/function.php
@@ -6,3 +6,13 @@ function greet($name) {
 }
 
 echo greet("Misbah");
+
+//optiona argument can not be place before required argument
+//function greetWithTime($time = "day", $name)
+function greetWithTime($name, $time = "day") {
+
+   return "Good $time, $name\n";
+}
+
+echo greetWithTime("Haque");
+echo greetWithTime("Sayem", "evening");
