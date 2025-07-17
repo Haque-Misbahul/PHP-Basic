8c57069fbffa033c1ba6f7ee8a8a23c2e02d609e
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-07-18 01:52:33 +0200

    generator

--- function/generator.php
+++ function/generator.php
@@ -1,13 +1,11 @@
 <?php
 
-function countDown(int $start): array {
+function countDown(int $start): Generator {
 
-   $result = [];
    for ($i = $start; $i > 0; $i--){
       echo "Generation number...\n";
-      $result[] = random_int(1, 100);
+      yield random_int(1, 100);
    }
-   return $result;
 }
 
 foreach(countDown(5) as $numbers) {
