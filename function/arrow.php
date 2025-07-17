9f7139d988bf06ca0533c4ffa81300b593515df8
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-07-18 01:15:23 +0200

    arrow function

---
+++ function/arrow.php
@@ -0,0 +1,18 @@
+<?php
+
+$numbers = [1,2,3];
+$multiplier = 3;
+
+$squared = array_map(function ($n) use($multiplier)  {
+
+   return $n * $multiplier;
+   
+}, $numbers);
+
+
+$squared2 = array_map(fn ($n) =>
+   $n * $multiplier,
+   $numbers
+);
+
+var_dump($numbers, $squared, $squared2);
