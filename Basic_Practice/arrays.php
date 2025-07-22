cfbf79fd9e82402ee8922097e23801710ee372f6
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-07-22 03:43:26 +0200

    array map and filter

--- Basic_Practice/arrays.php
+++ Basic_Practice/arrays.php
@@ -24,6 +24,26 @@ $fruits = ['apple', 'banana', 'orange'];
 
 var_dump(count($fruits));
 sort($fruits);
-var_dump($fruits);
+// var_dump($fruits);
 rsort($fruits);
-var_dump($fruits);
+// var_dump($fruits);
+
+// var_dump($assosiateiveArray);
+
+asort($assosiateiveArray);
+// var_dump($assosiateiveArray);
+ksort($assosiateiveArray);
+// var_dump($assosiateiveArray);
+
+$numbers = range(1,5);
+// var_dump($numbers);
+
+// in map variable at the end
+$squared = array_map(fn($n) => $n ** 2, $numbers);
+
+var_dump($squared);
+
+// in filter variable is at the beginning
+$evenNumbers = array_filter($numbers, fn($n) => $n % 2 ==0);
+
+var_dump($evenNumbers);
