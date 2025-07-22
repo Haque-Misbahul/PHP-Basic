70768bf1da2a2e95ec04a45410b80b93ef9d27a4
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-07-22 03:26:03 +0200

    arrays

---
+++ Basic_Practice/arrays.php
@@ -0,0 +1,11 @@
+<?php
+
+$simpleArray = [1,2,3,4,5];
+$assosiateiveArray = [
+   'name' => 'John',
+   'age' => 30,
+   'city' => 'New York'
+];
+
+echo $simpleArray[0];
+echo $assosiateiveArray['name'];
