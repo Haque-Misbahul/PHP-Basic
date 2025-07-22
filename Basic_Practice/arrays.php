c66b879769dfe6d2fd646417cd162288eb91c9c9
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-07-22 03:31:35 +0200

    array sort

--- Basic_Practice/arrays.php
+++ Basic_Practice/arrays.php
@@ -9,3 +9,21 @@ $assosiateiveArray = [
 
 echo $simpleArray[0];
 echo $assosiateiveArray['name'];
+
+$simpleArray[] = 6;
+
+$assosiateiveArray['country'] = 'USA';
+
+$matrix = [
+   [1,2,3],
+   [4,5,6]
+];
+echo $matrix[1][1];
+
+$fruits = ['apple', 'banana', 'orange'];
+
+var_dump(count($fruits));
+sort($fruits);
+var_dump($fruits);
+rsort($fruits);
+var_dump($fruits);
